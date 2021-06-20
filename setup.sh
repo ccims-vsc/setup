202fb67a6025ff7e06ed3a4232941bd243f08744
nk-coding <niklask.coding@gmail.com>
2021-06-20 16:28:39 +0200

    I hate space characters

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
-#! bin/sh
+#! /bin/sh
 set -e
 mkdir ccims-vsc
-docker run -it -v $(pwd):/home/ccims -w /home/ccims/ccims-vsc --entrypoint="" node:latest /bin/bash -c "git clone https://github.com/ccims-vsc/ccims-vsc-setup.git && chmod +x ./ccims-vsc-setup/setup.sh && ./ccims-vsc-setup/setup.sh"
+docker run -it -v "$(pwd):/home/ccims" -w /home/ccims/ccims-vsc --entrypoint="" node:latest /bin/bash -c "git clone https://github.com/ccims-vsc/ccims-vsc-setup.git && chmod +x ./ccims-vsc-setup/setup.sh && ./ccims-vsc-setup/setup.sh"
 docker-compose -f ccims-vsc/ccims-backend-gql/docker-compose.yaml up -d
 code --install-extension ccims-vsc/extension.vsix
 sudo chown -R $USER ccims-vsc/
