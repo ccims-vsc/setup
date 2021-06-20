745bc18bc51693e6c55fd93faa24ccf1da2685ef
nk-coding <niklask.coding@gmail.com>
2021-06-20 04:20:02 +0200

    docker does docker things

--- setup.sh
+++ setup.sh
@@ -4,4 +4,5 @@ mkdir ccims-vsc
 docker run -it -v $(pwd):/home/ccims -w /home/ccims/ccims-vsc --entrypoint="" node:latest /bin/bash -c "git clone https://github.com/ccims-vsc/ccims-vsc-setup.git && chmod +x ./ccims-vsc-setup/setup.sh && ./ccims-vsc-setup/setup.sh"
 docker-compose -f ccims-vsc/ccims-backend-gql/docker-compose.yaml up -d
 code --install-extension ccims-vsc/extension.vsix
+sudo chown -R $USER ccims-vsc/
 code ccims-vsc/example-project
