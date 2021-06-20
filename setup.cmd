fb4b54be1c9a74b61d10f801eec0bce2701ea63b
nk-coding <niklask.coding@gmail.com>
2021-06-20 02:22:43 +0200

    code is a batch file

--- setup.cmd
+++ setup.cmd
@@ -1,5 +1,5 @@
 mkdir ccims-vsc
 docker run -it -v %cd%:/home/ccims -w /home/ccims/ccims-vsc --entrypoint="" node:latest /bin/bash -c "git clone https://github.com/ccims-vsc/ccims-vsc-setup.git && chmod +x ./ccims-vsc-setup/setup.sh && ./ccims-vsc-setup/setup.sh"
 docker-compose -f ccims-vsc/ccims-backend-gql/docker-compose.yaml up -d
-code ccims-vsc/example-project
-code --install-extension ccims-vsc/extension.vsix
+call code --install-extension ccims-vsc/extension.vsix
+call code ccims-vsc/example-project
