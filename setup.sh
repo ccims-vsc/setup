452162b484e2dae6268a0434291e602d4c4cd77c
nk-coding <niklask.coding@gmail.com>
2021-06-22 18:29:31 +0200

    improved shell setup script

--- setup.sh
+++ setup.sh
@@ -1,8 +1,10 @@
 #! /bin/sh
 set -e
+rm -rf ccims-vsc
 mkdir ccims-vsc
 docker run -it -v "$(pwd):/home/ccims" -w /home/ccims/ccims-vsc --entrypoint="" node:latest /bin/bash -c "git clone https://github.com/ccims-vsc/ccims-vsc-setup.git && chmod +x ./ccims-vsc-setup/setup.sh && ./ccims-vsc-setup/setup.sh"
-docker-compose -f ccims-vsc/ccims-backend-gql/docker-compose.yaml up -d
+docker-compose -f ccims-vsc/ccims-backend-gql/docker-compose.yaml down -v
+docker-compose -f ccims-vsc/ccims-backend-gql/docker-compose.yaml up -d --build
 code --install-extension ccims-vsc/extension.vsix
 sudo chown -R $USER ccims-vsc/
 code ccims-vsc/example-project
