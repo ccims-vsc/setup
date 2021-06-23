ede71e632fe6550ca58c493f8e9b4f9c3f0b2cd3
nk-coding <niklask.coding@gmail.com>
2021-06-23 20:10:24 +0200

    added gropius branch support

--- setup.cmd
+++ setup.cmd
@@ -1,5 +1,5 @@
 mkdir ccims-vsc
-docker run -it -v %cd%:/home/ccims -w /home/ccims/ccims-vsc --entrypoint="" node:latest /bin/bash -c "git clone https://github.com/ccims-vsc/ccims-vsc-setup.git && chmod +x ./ccims-vsc-setup/setup.sh && ./ccims-vsc-setup/setup.sh"
+docker run -it -v %cd%:/home/ccims -w /home/ccims/ccims-vsc --entrypoint="" node:latest /bin/bash -c "git clone --single-branch --branch gropiusify https://github.com/ccims-vsc/ccims-vsc-setup.git && chmod +x ./ccims-vsc-setup/setup.sh && ./ccims-vsc-setup/setup.sh"
 docker-compose -f ccims-vsc/ccims-backend-gql/docker-compose.yaml down -v
 docker-compose -f ccims-vsc/ccims-backend-gql/docker-compose.yaml up -d --build
 call code --install-extension ccims-vsc/extension.vsix
--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 set -e
 rm -rf ccims-vsc
 mkdir ccims-vsc
-docker run -it -v "$(pwd):/home/ccims" -w /home/ccims/ccims-vsc --entrypoint="" node:latest /bin/bash -c "git clone https://github.com/ccims-vsc/ccims-vsc-setup.git && chmod +x ./ccims-vsc-setup/setup.sh && ./ccims-vsc-setup/setup.sh"
+docker run -it -v "$(pwd):/home/ccims" -w /home/ccims/ccims-vsc --entrypoint="" node:latest /bin/bash -c "git clone  --single-branch --branch gropiusify https://github.com/ccims-vsc/ccims-vsc-setup.git && chmod +x ./ccims-vsc-setup/setup.sh && ./ccims-vsc-setup/setup.sh"
 docker-compose -f ccims-vsc/ccims-backend-gql/docker-compose.yaml down -v
 docker-compose -f ccims-vsc/ccims-backend-gql/docker-compose.yaml up -d --build
 code --install-extension ccims-vsc/extension.vsix
